330b98c19b38df12f2f13b28b297fe582167cf6e
ericghw <ericghw@163.com>
2018-08-26 15:13:24 +0800

    改良版本一：封装store概念；监视数据变化自动刷新UI

--- src/index.js
+++ src/index.js
@@ -18,38 +18,64 @@ const appState = {
     }
 }
 
-function dispatch(action){
+function stateChange(state, action){
     switch(action.type){
         case 'UPDATE_HEADER_TEXT':
-            appState.header.text = action.text;
+            state.header.text = action.text;
             break;
         case 'UPDATE_HEADER_COLOR':
-            appState.header.color = action.color;
+            state.header.color = action.color;
             break;
         default:
             break;
     }
 }
 
+//用store的概念来封装state和action
+function createStore(state, stateChange){
+    var getState = () => state;
+
+    var listeners = [];
+    var subscribe = (renderState) => listeners.push(renderState);
+
+    var dispatch = (action) => {
+        stateChange(state, action);
+
+        listeners.forEach((listener) =>{
+            listener(state);
+        });
+    }
+
+    return {
+        getState,
+        dispatch,
+        subscribe,
+    };
+}
+
 function renderApp(state){
-    console.log('renderContent');
+    console.log('render App');
     renderHeader(state.header);
     renderContent(state.content);
 }
 
 function renderHeader(header){
+    console.log('render Header');
     var headerElem = document.getElementById('header');
     headerElem.innerHTML = header.text;
     headerElem.style.color = header.color;
 }
 
 function renderContent(content){
+    console.log('render Content');
     var contentElem = document.getElementById('content');
     contentElem.innerHTML = content.text;
     contentElem.style.color = content.color;
 }
 
-renderApp(appState);
-dispatch({type: 'UPDATE_HEADER_TEXT', text: '修改后的标题'});
-dispatch({type: 'UPDATE_HEADER_COLOR', color: 'green'});
-renderApp(appState);
+var store = createStore(appState, stateChange);
+store.subscribe((state) => renderApp(state));
+
+renderApp(store.getState());
+store.dispatch({type: 'UPDATE_HEADER_TEXT', text: '修改后的标题'});
+store.dispatch({type: 'UPDATE_HEADER_COLOR', color: 'green'});
